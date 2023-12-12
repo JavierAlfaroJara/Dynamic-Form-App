d068c5f14a51ca6e8b14c06793bbec390e3a0a92
Javier Alfaro Jara <javier.alfaro.jara@gmail.com>
2023-12-11 22:58:14 -0600

    Added new App.test.tsx unit test

--- dynamic-form-app/src/App.test.tsx
+++ dynamic-form-app/src/App.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import formReducer from './redux/formSlice';
+
+// Create Redux store
+const store = configureStore({
+  reducer: {
+    form: formReducer,
+  },
+});
+
+test('renders Dynamic Form link', () => {
+  render(
+      <Provider store={store}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Provider>
+    );
+  const linkElement = screen.getByText(/Dynamic Form/i);
   expect(linkElement).toBeInTheDocument();
 });
